4e93fe7cca701204d0019a57d51f4ae8a3947351
Alanderson Figueiredo <limaalanderson@gmail.com>
2024-10-18 21:44:46 -0300

    feat: manipular audio

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-//seleção de botão e elemento de saída
+// seleção de botões e elementos
 const startBtn = document.getElementById('start-btn');
 const output = document.getElementById('output');
+const playPauseBtn = document.getElementById('play-pause-btn');
+const audioElement = document.getElementById('audio'); 
 
-//inicializa a API de reconhecimento de voz (SpeechRecognition)
+// inicializa a API de reconhecimento de voz (SpeechRecognition)
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 recognition.lang = 'pt-BR';
 
-//evento de clique ao botão para iniciar reconhecimento de voz (SpeechRecognition)
+// evento de clique ao botão para iniciar reconhecimento de voz (SpeechRecognition)
 startBtn.addEventListener('click', () => {
     output.textContent = "Estou te ouvindo... Fale agora.";
     recognition.start(); //começa o reconhecimento de voz (SpeechRecognition)
 });
 
-//evento disparado quando o reconhecimento de voz captura uma frase
+// evento disparado quando o reconhecimento de voz captura uma frase
 recognition.onresult = (event) => {
     const transcript = event.results[0][0].transcript.toLowerCase();
     output.textContent = `Você disse: "${transcript}"`;
     interpretarComando(transcript);
 };
 
-//função para interpretar os comandos de voz
+// função para interpretar os comandos de voz
 function interpretarComando(comando) {
     if (comando.includes('tocar música')) {
         output.textContent = "Tocando música...";
@@ -35,6 +37,35 @@ function interpretarComando(comando) {
     }
 }
 
+function tocarMusica() {
+    const audioContext = new AudioContext();
+    // consegue o elemento de áudio
+    const track = audioContext.createMediaElementSource(audioElement);
+    track.connect(audioContext.destination);
 
+    // evento de clique no botão de play/pause
+    playPauseBtn.addEventListener('click', () => {
+        // verifica se o contexto está em estado suspenso 
+        if (audioContext.state === 'suspended') {
+            audioContext.resume();
+        }
+        // toca ou pausa a música dependendo do estado
+        if (playPauseBtn.dataset.playing === 'false') {
+            audioElement.play();
+            playPauseBtn.dataset.playing = 'true';
+            playPauseBtn.setAttribute('aria-checked', 'true');
+        } else if (playPauseBtn.dataset.playing === 'true') {
+            audioElement.pause();
+            playPauseBtn.dataset.playing = 'false';
+            playPauseBtn.setAttribute('aria-checked', 'false');
+        }
+    }, false);
+
+    // evento disparado quando a música termina
+    audioElement.addEventListener('ended', () => {
+        playPauseBtn.dataset.playing = 'false';
+        playPauseBtn.setAttribute('aria-checked', 'false');
+    }, false);
+}
 
 
